dd015e52f5d9cc86f3b280972b8b15eae263f69a
Teapotka <kalyvanivan33@gmail.com>
2022-12-04 00:50:26 +0100

    fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
 const app = express()
-
+const POST = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
     res.send({"status": "success"})
 })
-app.listen(3000, "localhost")
+app.listen(PORT)
